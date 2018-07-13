ce74dce574f5a5c0685a5f13f31459e98e7cdbc2
Ronny Cabrera <ronny-38@hotmail.com>
2018-07-12 23:30:06 -0500

    Conexion MySQL y Entidades

--- project/src/app.module.ts
+++ project/src/app.module.ts
@@ -6,9 +6,26 @@ import {LibroController} from "./libro/libro.controller";
 import {AutorizacionController} from "./controladores/autorizacion.controller";
 import {AutorService} from "./autor/autor.service";
 import {LibroService} from "./libro/libro.service";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {UsuarioEntity} from "./usuario/usuario.entity";
+import {AutorEntity} from "./autor/autor.entity";
+import {LibroEntity} from "./libro/libro.entity";
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: 'alweb.mysql.database.azure.com',
+          port: 3306,
+          username: 'ronny@alweb',
+          password: 'DAN300shanNY',
+          database: 'alweb',
+          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+          synchronize: true,
+          ssl :true
+      }),
+      TypeOrmModule.forFeature([UsuarioEntity, AutorEntity, LibroEntity])
+  ],
   controllers: [AppController, AutorController, LibroController, AutorizacionController],
   providers: [AppService, AutorService, LibroService],
 })
